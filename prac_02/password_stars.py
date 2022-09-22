00868fadff6730a89436d66d9e2abc7e645b2e49
zachm <zachm25404@outlook.com.au>
2022-09-22 21:54:41 +1000

    Refactor password check program to use functions

---
+++ prac_02/password_stars.py
@@ -0,0 +1,23 @@
+MINIMUM_LENGTH = 5
+
+def main():
+    """Main program."""
+    password = get_password()
+    prints_asterisks(password)
+
+
+def prints_asterisks(password):
+    for char in password:
+        print("*", end="")
+
+
+def get_password():
+    """Get a valid password."""
+    password = input("Enter password: ")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password needs to be atleast {MINIMUM_LENGTH} characters")
+        password = input("Enter password: ")
+    return password
+
+
+main()
