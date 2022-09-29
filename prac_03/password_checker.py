b2a9039662347979dd9b48621dcd26ec932c7818
zachm <zachm25404@outlook.com.au>
2022-09-29 19:28:25 +1000

    password checker program

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,26 +5,24 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = True
+SPECIAL_CHARS_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
     """Program to get and check a user's password."""
-    print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
+    print(f"Please enter a valid password")
+    print(f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:")
+    print(f"\t1 or more uppercase characters")
+    print(f"\t1 or more lowercase characters")
+    print(f"\t1 or more numbers")
     if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+        print(f"\tand 1 or more special characters: {SPECIAL_CHARACTERS}")
     password = input("> ")
     while not is_valid_password(password):
-        print("Invalid password!")
+        print(f"Invalid password!")
         password = input("> ")
-    print("Your {}-character password is valid: {}".format(len(password),
-                                                           password))
+    print(f"Your {len(password)}-character password is valid: {password}")
 
 
 def is_valid_password(password):
