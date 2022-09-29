ef0aa116202c91eb1b62e35284ce9aa6916ce7fb
zachm <zachm25404@outlook.com.au>
2022-09-29 17:52:03 +1000

    Exceptions demonstration

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,6 +9,9 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print(f"denominator cannot be 0!")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
