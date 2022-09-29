5badd5ff0963f69acda784215fa548dc25e92987
zachm <zachm25404@outlook.com.au>
2022-09-29 17:31:54 +1000

    Random module functions

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+# smallest: 5
+# largest: 20
+#
+# smallest: 3
+# largest: 9
+# 4 could not have been produced as it is even
+#
+# smallest: 2.5
+# largest: 5.5
+
+print(random.randint(1, 100))
