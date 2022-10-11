5f70f352c40395384236ac11040d4a4878a43817
zachm <zachm25404@outlook.com.au>
2022-10-11 12:14:28 +1000

    List comprehensions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+3
+2
+1
+3, 1, 4, 1, 5, 9
+1
+True
+False
+False
+3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+1. numbers[0] = "ten"
+2. numbers[-1] = 1
+3. print(numbers[2:])
+4. 9 in numbers
