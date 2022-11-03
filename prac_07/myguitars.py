3cd094ec287bd2173eab536c45c0e7b50e38597e
zachm <zachm25404@outlook.com.au>
2022-11-04 08:28:27 +1000

    Complete and working prac 7 More Guitars exercise.

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,17 +7,42 @@ Actual time taken - 20 minutes
 
 from prac_06.guitar import Guitar
 
+FILENAME = "guitars.csv"
+
 
 def main():
     guitars = []
-    # test = Guitar("Fender Stratocaster", 2014, 765.4)
-    with open("guitars.csv", "r") as in_file:
+    with open(FILENAME, "r+") as in_file:
         for line in in_file:
             parts = line.strip().split(",")
             guitars.append(Guitar(parts[0], int(parts[1]), float(parts[2])))
-        guitars.sort()
-        for guitar in guitars:
-            print(guitar)
+    add_guitars(guitars)
+    display_guitars(guitars)
+    save_file(guitars)
+
+
+def save_file(contents):
+    """Saves file content to a file passed to it"""
+    with open(FILENAME, "w", encoding="utf8") as out_file:
+        for content in contents:
+            print(f"{content}", file=out_file)
+
+
+def add_guitars(guitars):
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        add_guitar = Guitar(name, year, cost)
+        guitars.append(add_guitar)
+        print(f"{add_guitar} added.")
+        name = input("\nName: ")
+
+
+def display_guitars(guitars):
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
 
 
 main()
