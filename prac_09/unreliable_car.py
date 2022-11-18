c976b425dd9ba1e73a6787830172ff54677ce479
zachm <zachm25404@outlook.com.au>
2022-11-18 12:50:57 +1000

    Complete Unreliable Car class file exercise

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,17 @@
+from prac_09.car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    """Represent an unreliable car object"""
+    def __init__(self, reliability: float, **kwargs):
+        super().__init__(**kwargs)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Attempt to drive unreliable car a passed distance"""
+        random_number = randint(0, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        return super().drive(distance)
+
